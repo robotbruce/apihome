57913d886496921a3ce4b5320befb50576403d0b
bruce <bruceyu1113@gmail.com>
2021-01-16 10:34:59 +0800

    fix db_config

--- db_config.py
+++ db_config.py
@@ -1,21 +1,27 @@
 from flaskext.mysql import MySQL
 from flask import Flask
+import json
+
+with open(f"C:\\Users\\Public\\Documents\\db_config\\db_info.json") as json_file: 
+    db_info = json.load(json_file) 
+ 
+
 app = Flask(__name__)
 
 mysql = MySQL()
 
 # MySQL configurations
 def gcp_db():
-    app.config['MYSQL_DATABASE_USER'] = 'TVBS_NM'
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'frNsX7V@P@4mRg#8'
-    app.config['MYSQL_DATABASE_DB'] = 'MartServer'
-    app.config['MYSQL_DATABASE_HOST'] = '10.33.0.3'
+    app.config['MYSQL_DATABASE_USER'] = db_info['gcp']['username']
+    app.config['MYSQL_DATABASE_PASSWORD'] = db_info['gcp']['password']
+    app.config['MYSQL_DATABASE_DB'] = db_info['gcp']['db']
+    app.config['MYSQL_DATABASE_HOST'] = db_info['gcp']['host']
     mysql.init_app(app)
     
 
 def aws_db():
-    app.config['MYSQL_DATABASE_USER'] = 'tvbs'
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'tvbstvbs'
-    app.config['MYSQL_DATABASE_DB'] = 'tvbs_news_v4'
-    app.config['MYSQL_DATABASE_HOST'] = 'db41-ro-tvbs-aurora.clwuef820xta.ap-northeast-1.rds.amazonaws.com'
+    app.config['MYSQL_DATABASE_USER'] = db_info['aws']['username']
+    app.config['MYSQL_DATABASE_PASSWORD'] = db_info['aws']['password']
+    app.config['MYSQL_DATABASE_DB'] = db_info['aws']['db']
+    app.config['MYSQL_DATABASE_HOST'] = db_info['aws']['host']
     mysql.init_app(app)
